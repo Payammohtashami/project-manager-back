9c2af7f2f2d4bc160f9afcd272158050284d3433
Payam Mohtashami <p.mohtashami.1382.4@gmail.com>
2023-10-18 10:35:21 +0330

    ADD: Add edit profile

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,6 @@
+const omitEmpty = require('omit-empty');
+const { UserModel } = require("../../models/user.model");
+
 class UserController {
     getProfile(req, res, next){
         try {
@@ -11,8 +14,29 @@ class UserController {
         }
     };
 
-    editProfie(){
-
+    async editProfie(req, res, next){
+        try {
+            let data = omitEmpty(req.body);
+            const userId = req.user?._id;
+            let fields = ["first_name", "last_name", "skills"];
+            Object.entries(data).forEach(([key, value]) => {
+                if(!fields.includes(key)) delete data[key];
+            });
+            const result = await UserModel.updateOne({_id: userId}, {$set: data});
+            if(result.modifiedCount > 0) {
+                return res.status(200).json({
+                    status: 200,
+                    message: "به روز رسانی پروفایل با موفقیت انجام شد",
+                    data,
+                });
+            };
+            throw {
+                status: 400,
+                message: "به روز رسانی انجام نشد",
+            };
+        } catch (error) {
+            next(error);
+        }
     };
 
     addSkilles(){
--- app/routers/user.routes.js
+++ app/routers/user.routes.js
@@ -4,4 +4,5 @@ const checkLogin = require('../http/middlewares/autoLogin');
 const router = require('express').Router();
 
 router.get('/profile', checkLogin, UserController.getProfile);
+router.put('/profile', checkLogin, UserController.editProfie);
 module.exports = router;
